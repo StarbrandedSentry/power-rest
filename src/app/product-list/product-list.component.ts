862254bbe7f329bca1d9b31fc275fee047109743
Ciaossuuu <55319885+Ciaossuuu@users.noreply.github.com>
2019-11-09 22:04:35 +0800

    api 😱😱😱✊

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,16 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProductListComponent implements OnInit {
+  public appPages = [
+    {
+      title: 'Accounts',
+      url: './../../assets/images/accounts.png'
+    },
 
-  items: Array<any> = [
-    // {subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
-    {subject: 'Accounts'},
-    {subject: 'PhilAm Life Products'},
-    {subject: 'PhilAm Life Vitality'},
-    {subject: 'Reward'},
-    {subject: 'Payment'},
-    {subject: 'Research Product'},
+    {
+      title: 'PhilAm Life Products',
+      url: './../../assets/images/products.jpg',
+    },
+  
+    {
+      title: 'PhilAm Life Vitality',
+      url: './../../assets/images/vitality.jpg'
+    },
+    {
+      title: 'Reward',
+      url: './../../assets/images/rewards.png'
+    }
+    ,
+    {
+      title: 'Payment',
+      url: './../../assets/images/payments.png',
+    },
+    {
+      title: 'Research Product',
+      url: '/contribution',
+    }
   ];
+
+  // items: Array<any> = [
+  //   {subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
+  //   {subject: 'Accounts'},
+  //   {subject: 'PhilAm Life Products'},
+  //   {subject: 'PhilAm Life Vitality'},
+  //   {subject: 'Reward'},
+  //   {subject: 'Payment'},
+  //   {subject: 'Research Product'},
+  // ];
 
   constructor() { }
 
