5a0a746ceca24d2eace06445f6d5da98bb4b634a
Ciaossuuu <55319885+Ciaossuuu@users.noreply.github.com>
2019-11-09 17:19:41 +0800

    box end 😏
    xxxx

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
+
 export class ProductListComponent implements OnInit {
+
+  items: Array<any> = [
+    // {subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
+    {subject: 'Accounts'},
+    {subject: 'PhilAm Life Products'},
+    {subject: 'PhilAm Life Vitality'},
+    {subject: 'Reward'},
+    {subject: 'Payment'},
+    {subject: 'Research Product'},
+  ];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
